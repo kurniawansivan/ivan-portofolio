a10f343d20ea215c8af7adc390a364f29bde55e7
kurniawansivan <ivan@wolkk.com>
2025-11-02 20:51:24 +0800

    fix: update navbar for improved menu toggle functionality and correct name display

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,40 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
+const isMenuOpen = ref(false)
 </script>
 
 <template>
   <header>
     <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
-      <!-- <div class="navbar-brand">
+      <div class="navbar-brand">
         <RouterLink class="navbar-item" to="/">
-          <strong>Portofolio Saya</strong>
+          <strong>Ivan Kurniawan Sentosa</strong>
         </RouterLink>
-      </div> -->
-      <div class="navbar-menu">
+
+        <a role="button" class="navbar-burger" :class="{ 'is-active': isMenuOpen }" @click="isMenuOpen = !isMenuOpen"
+          aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu" :class="{ 'is-active': isMenuOpen }">
         <div class="navbar-start">
-          <RouterLink class="navbar-item" to="/">Home</RouterLink>
-          <RouterLink class="navbar-item" to="/projects">Proyek</RouterLink>
-          <RouterLink class="navbar-item" to="/blog">Blog</RouterLink>
-          <RouterLink class="navbar-item" to="/contact">Contact</RouterLink>
+          <RouterLink class="navbar-item" to="/" @click="isMenuOpen = false">
+            Home
+          </RouterLink>
+          <RouterLink class="navbar-item" to="/projects" @click="isMenuOpen = false">
+            Proyek
+          </RouterLink>
+          <RouterLink class="navbar-item" to="/blog" @click="isMenuOpen = false">
+            Blog
+          </RouterLink>
+          <RouterLink class="navbar-item" to="/contact" @click="isMenuOpen = false">
+            Contact
+          </RouterLink>
         </div>
       </div>
     </nav>
@@ -40,6 +59,10 @@ import { RouterLink, RouterView } from 'vue-router'
 .router-link-active {
   background-color: #4a4a4a;
   font-weight: bold;
+  color: white;
+}
+
+.navbar-item.router-link-active {
   color: white;
 }
 </style>
