32f658bf5aeab76b53cad0f9c6828c686a461b9e
kurniawansivan <ivan@wolkk.com>
2025-11-03 09:55:13 +0800

    feat: add new projects and images for Deinzer Gallery Management System and related applications

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,23 +3,23 @@ import type { Project } from '@/types/Project';
 export const projects: Project[] = [
     {
         id: 1,
-        title: 'Personal Portfolio',
-        shortDescription: 'A personal portfolio website to showcase my projects and skills.',
-        image: 'https://placehold.co/300x200/cccccc/969696.png?text=Image&font=lato',
-        tags: ['Vue 3', 'TypeScript', 'Bulma'],
+        title: 'Deinzer Gallery Managament System',
+        shortDescription: 'I built the complete backend for a high-stakes internal management system, automating everything from complex artist schedules and user roles to financial reporting and high-resolution image uploads.',
+        image: '../src/assets/images/deinzer-1.png',
+        tags: ['Python', 'FastAPI', 'PostgreSQL', 'Docker'],
     },
     {
         id: 2,
-        title: 'E-commerce Platform',
-        shortDescription: 'An e-commerce platform with shopping cart and payment integration.',
-        image: 'https://placehold.co/300x200/cccccc/969696.png?text=Image&font=lato',
-        tags: ['React', 'Node.js', 'MongoDB'],
+        title: 'Deinzer Smart Seller',
+        shortDescription: 'I engineered the backend and recommendation logic for an innovative smart-shelf, powering real-time product suggestions. This project was successfully pitched to industry leaders Ferrero Group, Rossmann and Eurocis 2024.',
+        image: '../src/assets/images/deinzer-2.jpeg',
+        tags: ['Python', 'FastAPI', 'Azure Blob Storage', 'Recommendation Systems'],
     },
     {
         id: 3,
-        title: 'Blog Application',
-        shortDescription: 'A full-featured blog application with user authentication and comments.',
-        image: 'https://placehold.co/300x200/cccccc/969696.png?text=Image&font=lato',
-        tags: ['Django', 'Python', 'SQLite'],
+        title: 'JB iphone Warehouse',
+        shortDescription: 'I developed a custom, all-in-one mobile app to solve a critical business problem. This full-stack solution gives an iPhone store real-time, on-the-go control over their entire warehouse, from stocking to instant profit calculation.',
+        image: '../src/assets/images/deinzer-3.png',
+        tags: ['Flutter', 'Python', 'SQLite', 'Full-Stack'],
     }
 ]
